301147625adf7851cd1e48dbaf77f2b0bbf721d6
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-02-14 22:34:23 -0300

    add totalCost field to Listing type

--- subgraph-listings/resolvers.js
+++ subgraph-listings/resolvers.js
@@ -7,6 +7,10 @@ const resolvers = {
     Listing: {
         host: ({ hostId }) => {
             return { id: hostId };
+        },
+        totalCost: async ({ id }, { checkInDate, checkOutDate }, { dataSources }) => {
+            const { totalCost } = await dataSources.listingsAPI.getTotalCost({ id, checkInDate, checkOutDate });
+            return totalCost || 0;
         }
     }
 };
