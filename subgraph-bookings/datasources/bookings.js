82aa969b4f3bded45cb0e5b4bbdfb95e5b2da63b
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-02-14 22:36:13 -0300

    add type to booking schema

--- subgraph-bookings/datasources/bookings.js
+++ subgraph-bookings/datasources/bookings.js
@@ -13,11 +13,12 @@ class BookingsDb {
         const config = {
             username: 'root',
             password: null,
-            database: 'database_development',
+            database: 'bookings',
             dialect: 'sqlite',
             storage: './../services/bookings/bookings.db', // path to the bookings database file, relative to where this datasource is initialized,
-            logging: false, // set this to true if you want to see logging output in the terminal console
+            logging: true, // set this to true if you want to see logging output in the terminal console
         };
+
         const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
         const db = {};
--- subgraph-bookings/resolvers.js
+++ subgraph-bookings/resolvers.js
@@ -3,8 +3,40 @@ const { AuthenticationError, ForbiddenError } = require('./utils/errors');
 const resolvers = {
   // TODO: fill in resolvers
   Query: {
-    example: () => 'Hello World!',
+    bookings: (_, __, { dataSources, userId }) => {
+      return dataSources.bookingDb.getBookingsForUser(userId)
+    },
   },
+  Booking: {
+    __resolveReference: (bookingId, { dataSources }) => {
+      return dataSources.bookingDb.getBooking(bookingId);
+    },
+    checkInDate: ({ checkInDate }, _, { dataSources }) => {
+      return dataSources.bookingDb.getHumanReadableDate(checkInDate);
+    },
+    checkOutDate: ({ checkOutDate }, _, { dataSources }) => {
+      return dataSources.bookingDb.getHumanReadableDate(checkOutDate);
+    }
+  },
+  Listing: {
+    bookings: async ({ id }, _, { dataSources }) => {
+      return await dataSources.bookingDb.getBookingsForListing(id)
+    },
+    numberOfUpcomingBookings: async ({ id }, __, { dataSources }) => {
+      return ((await dataSources.bookingDb.getBookingsForListing(id, 'UPCOMING')) || []).length;
+    },
+    currentlyBookedDates: async ({ id }, _, { dataSources }) => {
+      return await dataSources.bookingDb.getCurrentlyBookedDateRangesForListing(id);
+    }
+  },
+  ReservedDate: {
+    checkInDate: ({ checkInDate }, _, { dataSources }) => {
+      return dataSources.bookingDb.getHumanReadableDate(checkInDate);
+    },
+    checkOutDate: ({ checkOutDate }, _, { dataSources }) => {
+      return dataSources.bookingDb.getHumanReadableDate(checkOutDate);
+    }
+  }
 };
 
 module.exports = resolvers;
