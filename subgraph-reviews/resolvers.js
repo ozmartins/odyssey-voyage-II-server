940d5b3e6f3a8eff022bee898990ce80c3222238
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-02-16 19:46:49 -0300

    remove unused queries and fields from review subgraph

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,6 +1,11 @@
 const { AuthenticationError, ForbiddenError } = require('./utils/errors');
 
 const resolvers = {
+  Query: {
+    myReviews: (_, __, { dataSources, userId }) => {
+      return dataSources.reviewsDb.getReviewsByUser(userId);
+    }
+  },
   Listing: {
     reviews: ({ id }, _, { dataSources }) => {
       return dataSources.reviewsDb.getReviewsForListing(id);
